5ab8cb089ea7e7c49d94259094d18fd0537038dc
Lakshamana <guitrompa1@gmail.com>
2020-04-27 00:14:21 -0300

    separe contructors signature from impl

--- structs.cpp
+++ structs.cpp
@@ -6,20 +6,12 @@ typedef struct Person {
     char* name;
     int age;
 
-    Person(char *name, int age) {
-        this->name = name;
-        this->age = age;
-    }
+    // Method/constructor impl can be out of this context
+    Person(char *name, int age);
 
-    Person(char *name) {
-        this->name = name;
-        this->age = 0;
-    }
+    Person(char *name);
 
-    Person() {
-        this->name = "";
-        this->age = 0;
-    }
+    Person();
 
     void set_name(char *name) {
         this->name = name;
@@ -30,13 +22,30 @@ typedef struct Person {
     }
 };
 
+// Use scope resolution operator (::)
+// to indicate of what context Person's 
+// constructor belongs to
+Person::Person(char *name, int age) {
+    this->name = name;
+    this->age = age;
+}
+
+Person::Person(char *name) {
+    this->name = name;
+    this->age = 0;
+}
+
+Person::Person() {
+    this->name = "";
+    this->age = 0;
+}
 
 int main(int argc, char *argv[]) {
     Person person("Lakshamana", 22);
 
     cout << person.name << endl;
     cout << person.age << endl;
-    cout << "With pointers:" << endl;
+    cout << "\nWith pointers:" << endl;
 
     Person *p = &person;
     cout << p->name << endl;

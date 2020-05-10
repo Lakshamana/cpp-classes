4bb4ebc358a922012d77f5b70ca08fc30e49c6a0
Lakshamana <guitrompa1@gmail.com>
2020-05-09 22:27:18 -0300

    finish linked list

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -21,6 +21,10 @@ class Node {
       return value;
     }
 
+    void set_value(t value) {
+      this->value = value;
+    }
+
     Node* get_next() {
       return next;
     }
@@ -33,14 +37,17 @@ class Node {
 template<typename t>
 class List {
   Node<t>* root;
+  Node<t>* tail;
 
   public:
     List() {
       root = nullptr;
+      tail = nullptr;
     }
 
     List(t value) {
       root = new Node<t>(value);
+      tail = root;
     }
 
     virtual ~List() {
@@ -61,13 +68,11 @@ class List {
       Node<t>* node = new Node<t>(value);
       if (empty()) {
         root = node;
+        tail = root;
         return;
       }
-      Node<t>* last = root;
-      while (last->get_next()) {
-        last = last->get_next();
-      }
-      last->set_next(node);
+      tail->set_next(node);
+      tail = node;
     }
 
     void insert_at(int pos, t value) {
@@ -84,6 +89,14 @@ class List {
       node->set_next(next);
     }
 
+    t first() {
+      return root->get_value();
+    }
+
+    t last() {
+      return tail->get_value();
+    }
+
     t get(int index) {
       int i = 0;
       Node<t>* node = root;
@@ -92,6 +105,100 @@ class List {
         i++;
       }
       return node->get_value();
+    }
+
+    void remove_head() {
+      if (empty()) return;
+      Node<t>* node = root;
+      root = node->get_next();
+      node->set_next(nullptr);
+      delete node;
+      node = nullptr;
+    }
+
+    void remove_tail() {
+      if (empty()) return;
+      Node<t>* prev = root;
+      while (prev->get_next() != tail) {
+        prev = prev->get_next();
+      }
+      Node<t>* node = tail;
+      tail = prev;
+      tail->set_next(nullptr);
+      delete node;
+      node = nullptr;
+    }
+
+    void remove_at(int index) {
+      if (empty()) return;
+      int i = 0;
+      Node<t>* previous = root;
+      Node<t>* rmvNode = root;
+      while (i < index) {
+        previous = rmvNode;
+        rmvNode = rmvNode->get_next();
+        i++;
+      }
+      previous->set_next(rmvNode->get_next());
+      rmvNode->set_next(nullptr);
+      delete rmvNode;
+      rmvNode = nullptr;
+    }
+
+    int length() {
+      int i = 0;
+      if (empty()) return 0;
+      Node<t>* node = root;
+      while (node) {
+        node = node->get_next();
+        i++;
+      }
+      return i;
+    }
+
+    bool exists(t value) {
+      Node<t>* node = root;
+      while (node) {
+        if (node->get_value() == value)
+          return true;
+        node = node->get_next();
+      }
+      return false;
+    }
+
+    void replace(int index, t value) {
+      int i = 0;
+      Node<t>* node = root;
+      while (i < index) {
+        node = node->get_next();
+        i++;
+      }
+      node->set_value(value);
+    }
+
+    int index(t value) {
+      int i = 0;
+      Node<t>* node = root;
+      while (node) {
+        if (node->get_value() == value)
+          return i;
+        node = node->get_next();
+        i++;
+      }
+      return -1;
+    }
+
+    int last_index(t value) {
+      int i = 0;
+      int last = -1;
+      Node<t>* node = root;
+      while (node) {
+        if (node->get_value() == value)
+          last = i;
+        node = node->get_next();
+        i++;
+      }
+      return last;
     }
 
     void print() {
@@ -114,7 +221,30 @@ int main(int argc, char *argv[]) {
   l.print();
 
   int i = 2;
-  printf("\nValue at %i: ", i);
+  printf("Value at %i: ", i);
   cout << l.get(i) << endl;
+
+  cout << "\nList after remove 2nd index el & filling it with '2':" << endl;
+  l.remove_at(2); // remove
+  l.insert_at(2, 2); // insert
+  l.print();
+
+  l.remove_head();
+  l.remove_tail();
+  l.print();
+
+  cout << "\nExists item = 3? " << (l.exists(3) ? "y" : "n") << endl;
+  cout << "First: " << l.first() << endl;
+  cout << "Last: " << l.last() << endl;
+
+  l.insert_tail(1);
+  l.insert_tail(3);
+  cout << endl;
+  l.print();
+  cout << "First index of 3: " << l.index(3) << endl;
+  cout << "Last index of 3: " << l.last_index(3) << endl;
+  cout << "First index of 10: " << l.index(10) << endl;
+  cout << "Last index of 7: " << l.last_index(7) << endl;
+  cout << "\nList final length: " << l.length() << endl;
   return 0;
 }

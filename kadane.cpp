626021eebca2d27895c3bffdd667bf93c9ffda56
Lakshamana <guitrompa1@gmail.com>
2020-08-08 17:31:41 -0300

    add kadane

---
+++ kadane.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int kadane(vector<int>& v) {
+  int curr_max = 0, total_max = -1;
+  int size_v = v.size();
+  for (int i = 0; i < size_v; ++i) {
+    curr_max += v[i];
+    if (curr_max < 0)
+      curr_max = 0;
+    if (curr_max > total_max) {
+      total_max = curr_max;
+    }
+  }
+  return total_max;
+}
+
+int main(int argc, char *argv[]) {
+  vector<int> v;
+  int numbers[] = {-2, 5, -1, 2, -3};
+  v.assign(numbers, numbers + 5);
+
+  cout << "Max sum of numbers: " << kadane(v) << '\n';
+  return 0;
+}

2fc91a1481aab39d83962e7714bb4c631221919a
Lakshamana <lakshamana@protonmail.com>
2022-04-01 20:26:30 -0300

    chore: update compile script

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 out=`echo $1 | tr . ' ' | awk '{print $1}'`
-g++ $1 -o "$out.o" $2
+g++ $1 -o "build/$out" $2
 
 # Use g++ -o [name].o [..].cpp -Wall 
 # in order to compile with all of 
